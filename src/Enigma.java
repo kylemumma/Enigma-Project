c0107e0e5d318cbd1a061882c5f97f332eaaffe6
superkyle848 <kylemumma@apples-air.nsd.org>
2017-05-22 08:53:33 -0700

    Checkpoint 1, 2, 3: Basic Caesar, Advanced Caesar, and Affine (all together in one program)

--- src/Enigma.java
+++ src/Enigma.java
@@ -15,10 +15,22 @@ public class Enigma {
 			String command = scan.next(); //put first word of input in var
 			
 			//decides what to do based on user command (encrypt, decrypt, or error)
-			if(command.equalsIgnoreCase("ENCRYPT:")){
-				System.out.println(encrypt(scan.nextLine()));
-			} else if(command.equalsIgnoreCase("DECRYPT:")){
-				System.out.println(decrypt(scan.nextLine()));
+			if(command.equalsIgnoreCase("BASICCAESARENCRYPT:")){
+				System.out.println(basicCaesarEncrypt(scan.nextLine()));
+			} else if (command.equalsIgnoreCase("BASICCAESARDECRYPT:")) {
+				System.out.println(basicCaesarDecrypt(scan.nextLine()));
+			} else if(command.contains("ADVANCEDCAESARENCRYPT")){
+				String shiftStr = scan.next();
+				int shiftAmount = Integer.parseInt(shiftStr.substring(0, shiftStr.length() - 1));
+				System.out.println(advancedCaesarEncrypt(scan.nextLine(), shiftAmount));
+			} else if(command.contains("ADVANCEDCAESARDECRYPT")) {
+				String shiftStr = scan.next();
+				int shiftAmount = Integer.parseInt(shiftStr.substring(0, shiftStr.length() - 1));
+				System.out.println(advancedCaesarDecrypt(scan.nextLine(), shiftAmount));
+			} else if(command.equalsIgnoreCase("AFFINEENCRYPT:")){
+				System.out.println(affineEncrypt(scan.nextLine()));
+			} else if(command.equalsIgnoreCase("AFFINEDECRYPT:")){
+				System.out.println(affineDecrypt(scan.nextLine()));
 			} else {
 				System.out.println("Invalid input");
 				scan.nextLine();
@@ -27,7 +39,63 @@ public class Enigma {
 		
 	}
 	
-	public static String encrypt(String message){
+	public static String basicCaesarEncrypt(String message){
+		String newMessage = "";
+		for(int i = 1; i < message.length(); i++){
+			int charInt = message.charAt(i) + 1;
+			if(charInt > 90){ //if it goes past z, bring it back to a
+				charInt -= 26;
+			} else if(charInt == 33){ //if its a space leave it
+				charInt = 32;
+			}
+			newMessage += (char) charInt;
+		}
+		return newMessage;
+	}
+	
+	public static String basicCaesarDecrypt(String message){
+		String newMessage = "";
+		for(int i = 1; i < message.length(); i++){
+			int charInt = message.charAt(i) - 1;
+			if(charInt < 65 && charInt != 31){ //if it goes before a, bring it up to z
+				charInt += 26;
+			} else if(charInt == 31){ //if its a space leave it
+				charInt = 32;
+			}
+			newMessage += (char) charInt;
+		}
+		return newMessage;
+	}
+	
+	public static String advancedCaesarEncrypt(String message, int shiftAmount){
+		String newMessage = "";
+		for(int i = 1; i < message.length(); i++){
+			int charInt = message.charAt(i) + shiftAmount;
+			if(charInt > 90){ //if it goes past z, bring it back to a
+				charInt -= 26;
+			} else if(charInt == 32 + shiftAmount){ //if its a space leave it
+				charInt = 32;
+			}
+			newMessage += (char) charInt;
+		}
+		return newMessage;
+	}
+	
+	public static String advancedCaesarDecrypt(String message, int shiftAmount){
+		String newMessage = "";
+		for(int i = 1; i < message.length(); i++){
+			int charInt = message.charAt(i) - shiftAmount;
+			if(charInt < 65 && charInt != (32 - shiftAmount)){ //if it goes before a, bring it up to z
+				charInt += 26;
+			} else if(charInt == 32 - shiftAmount){ //if its a space leave it
+				charInt = 32;
+			}
+			newMessage += (char) charInt;
+		}
+		return newMessage;
+	}
+	
+	public static String affineEncrypt(String message){
 		String encryptMap = "QWERTYUIOPASDFGHJKLZXCVBNM";
 		String newMessage = "";
 		for(int i = 1; i < message.length(); i++){
@@ -45,7 +113,7 @@ public class Enigma {
 		return newMessage;
 	}
 
-	public static String decrypt(String message){
+	public static String affineDecrypt(String message){
 		String encryptMap = "QWERTYUIOPASDFGHJKLZXCVBNM";
 		String newMessage = "";
 		for(int i = 1; i < message.length(); i++){
